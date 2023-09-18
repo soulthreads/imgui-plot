73acf4f228e3876698db6ae9d7768bad33fd9e6a
Chadwick Boulay <chadwick.boulay@gmail.com>
2023-09-18 13:13:34 -0400

    Move IMGUI_DEFINE_MATH_OPERATORS to before first #include <imgui.h> as required by upstream a1b8457

--- include/imgui_plot.h
+++ include/imgui_plot.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <cstdint>
+#ifndef IMGUI_DEFINE_MATH_OPERATORS
+#define IMGUI_DEFINE_MATH_OPERATORS
+#endif
 #include <imgui.h>
 
 namespace ImGui {
--- src/imgui_plot.cpp
+++ src/imgui_plot.cpp
@@ -1,8 +1,5 @@
 #include <imgui_plot.h>
 #include <imgui.h>
-#ifndef IMGUI_DEFINE_MATH_OPERATORS
-#define IMGUI_DEFINE_MATH_OPERATORS
-#endif
 #include <imgui_internal.h>
 
 namespace ImGui {
